bf81f11865027a01e5ea19ec73144745ce449af8
Arie Milner <amilner42@gmail.com>
2017-02-17 16:32:41 -0800

    Closes #57 - Interesting case of setTimeout 0 being helpful, I guess the   requestAnimationFrame immedietely takes over and doesn't even   let the initial paint of the DOM happen, the DOM ends up   being too short, it does the full scroll to the too-short DOM,   on the other hand if we do a setTimeout 0, the DOM gets the inital   paint, THEN the requestAnimationFrame blocks future repaints and   does the scroll, but the initial paint is crucial to get the   full height

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -52,43 +52,45 @@ function getElementY(querySelector) {
 
 // For smooth scrolling, @refer: https://jsfiddle.net/s61x7c4e/
 app.ports.doScrolling.subscribe(function(scrollConfig) {
-  var startingY = window.pageYOffset;
-  var elementY = getElementY(scrollConfig.querySelector);
-  // If element is close to page's bottom then window will scroll only to some position above the element.
-  var targetY = document.body.scrollHeight - elementY < window.innerHeight ? document.body.scrollHeight - window.innerHeight : elementY;
-  var diff = targetY - startingY;
-  // Easing function: easeInOutCubic
-  // From: https://gist.github.com/gre/1650294
-  var easing = function (t) {
-    return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1
-  };
-
-  var start;
-
-  if (!diff) {
-    return;
-  }
-
-  // Bootstrap our animation - it will get called right before next frame shall be rendered.
-  window.requestAnimationFrame(function step(timestamp) {
-    if (!start) {
-      start = timestamp
-    }
-    // Elapsed miliseconds since start of scrolling.
-    var time = timestamp - start;
-  // Get percent of completion in range [0, 1].
-    var percent = Math.min(time / scrollConfig.duration, 1);
-    // Apply the easing.
-    // It can cause bad-looking slow frames in browser performance tool, so be careful.
-    percent = easing(percent);
-
-    window.scrollTo(0, startingY + diff * percent);
-
-  // Proceed with animation as long as we wanted it to.
-    if (time < scrollConfig.duration) {
-      window.requestAnimationFrame(step)
+  setTimeout(() => {
+    var startingY = window.pageYOffset;
+    var elementY = getElementY(scrollConfig.querySelector);
+    // If element is close to page's bottom then window will scroll only to some position above the element.
+    var targetY = document.body.scrollHeight - elementY < window.innerHeight ? document.body.scrollHeight - window.innerHeight : elementY;
+    var diff = targetY - startingY;
+    // Easing function: easeInOutCubic
+    // From: https://gist.github.com/gre/1650294
+    var easing = function (t) {
+      return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1
     };
-  });
+
+    var start;
+
+    if (!diff) {
+      return;
+    }
+
+    // Bootstrap our animation - it will get called right before next frame shall be rendered.
+    window.requestAnimationFrame(function step(timestamp) {
+      if (!start) {
+        start = timestamp
+      }
+      // Elapsed miliseconds since start of scrolling.
+      var time = timestamp - start;
+    // Get percent of completion in range [0, 1].
+      var percent = Math.min(time / scrollConfig.duration, 1);
+      // Apply the easing.
+      // It can cause bad-looking slow frames in browser performance tool, so be careful.
+      percent = easing(percent);
+
+      window.scrollTo(0, startingY + diff * percent);
+
+    // Proceed with animation as long as we wanted it to.
+      if (time < scrollConfig.duration) {
+        window.requestAnimationFrame(step)
+      };
+    });
+  }, 0);
 });
 
 // Creates the code editor by embedding into the element with the correct id.
