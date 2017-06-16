db37efb4390529ce656bff68d8326d5876322dae
Arie Milner <amilner42@gmail.com>
2017-06-15 17:21:49 -0700

    Updated migration script to also init QA objects for existing tidbits - Closes #175

--- backend/migrations/3-qa-indexes.migration.js
+++ backend/migrations/3-qa-indexes.migration.js
@@ -1,4 +1,27 @@
-/// 3rd migration file - Add indexes for optimizing retreiving QA objects for snipbits/bigbits.
+/// 3rd migration file - Add indexes for optimizing retreiving QA objects for snipbits/bigbits. Additionally, go
+/// through all snipbits/bigbits and initialize the default QA objects.
 
 db.snipbitsQA.createIndex({ tidbitID: 1 });
 db.bigbitsQA.createIndex({ tidbitID: 1 });
+
+// Creates the default QAObject.
+var defaultQAObject = function(tidbitID, tidbitAuthor) {
+    return {
+        tidbitID: tidbitID,
+        tidbitAuthor: tidbitAuthor,
+        questions: [],
+        questionComments: [],
+        answers: [],
+        answerComments: []
+    }
+};
+
+// Init QA objects for all snipbits.
+db.snipbits.find().forEach((snipbit) => {
+    db.snipbitsQA.insertOne(defaultQAObject(snipbit._id, snipbit.author));
+});
+
+// Init QA objects for all bigbits.
+db.bigbits.find().forEach((bigbit) => {
+    db.bigbitsQA.insertOne(defaultQAObject(bigbit._id, bigbit.author));
+});
